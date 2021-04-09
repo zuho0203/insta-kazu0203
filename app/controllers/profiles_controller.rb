11a608ba55d4ffb9f9e16f033a9cb512f6cac804
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-04-09 15:26:18 +0900

    画像投稿機能

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,6 +7,17 @@ class ProfilesController < ApplicationController
         @profile = current_user.prepare_profile
     end
 
+    def update
+        @profile = current_user.prepare_profile
+        @profile.assign_attributes(profile_params)
+        if @profile.save
+            redirect_to profile_path, notice: 'プロフィール更新！'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
     private
     def profile_params
         params.require(:profile).permit(

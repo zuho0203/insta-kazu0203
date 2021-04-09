17bbcaa05f00d0d957798e533ea2e2ce9582384e
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-04-09 15:04:46 +0900

    profile編集画面

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,21 @@ class ProfilesController < ApplicationController
     def show
         @account = current_user.account
     end
+
+    def edit
+        @profile = current_user.prepare_profile
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(
+            :nickname,
+            :introduction,
+            :gender,
+            :birthday,
+            :subscribed,
+            :avatar
+
+        )
+    end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,18 @@
 class Profile < ApplicationRecord
+    enum gender: { male: 0, female:1, other: 2 }
+    belongs_to :user
+    has_one_attached :avatar
+
+
+    def age
+        return '不明' unless birthday.present?
+        years = Time.zone.now.year - birthday.year
+        days = Time.zone.now.yday - birthday.yday
+
+        if days < 0
+            "#{years -1}歳"
+        else
+            "#{years}歳"
+        end
+    end
 end

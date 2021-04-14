d830d23b88a9cc67c99c6c30e174e5808c2b203d
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-04-14 12:23:21 +0900

    Commentモデルにユーザidを追加して、コメント投稿機能(rails)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,25 +5,28 @@ class CommentsController < ApplicationController
     end
 
     def index
-        article = Article.find(params[:article_id])
-        comments = article.comments
-        render json: comments
+      article = Article.find(params[:article_id])
+      comments = article.comments
+
+      render json: comments
+      
     end
 
     def create
-        article = Article.find(params[:article_id])
-        @comment = article.comments.build(comment_params)
-        if @comment.save
-          redirect_to article_path(article), notice: 'コメントを追加'
-        else
-          flash.now[:error] = '更新できませんでした'
-          render :new
-        end
+      article = Article.find(params[:article_id])
+
+      @comment = article.comments.build(comment_params)
+      if @comment.save
+        redirect_to article_path(article), notice: 'コメントを追加'
+      else
+        flash.now[:error] = '更新できませんでした'
+        render :new
       end
+    end
     
       private
       def comment_params
-        params.require(:comment).permit(:content)
+        params.require(:comment).permit(:content).merge(user_id: current_user.id)
       end
       
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
 
   def prepare_profile
     profile || build_profile

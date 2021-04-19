ba14def1a86413d734a4d495cfa3dc19029cbd2e
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-04-19 12:57:01 +0900

    フォローアンフォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,12 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :avatar, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
 
   def prepare_profile
     profile || build_profile
@@ -30,4 +36,30 @@ class User < ApplicationRecord
   def has_liked?(article)
     likes.exists?(article_id: article.id)
   end
+
+  def follow!(user)
+    user_id = get_user_id(user)
+    following_relationships.create!(following_id: user_id)
+  end
+
+  def unfollow!(user)
+    user_id = get_user_id(user)
+
+    relation = following_relationships.find_by!(following_id: user_id)
+    relation.destroy!
+  end
+
+  def has_followed?(user)
+    following_relationships.exists?(following_id: user.id)
+  end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
+    end
+  end
+
 end

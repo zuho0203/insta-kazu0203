e2ef942c5d078f41bac9466213a582bfb7755fe5
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-04-19 09:23:42 +0900

    sidekiq実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

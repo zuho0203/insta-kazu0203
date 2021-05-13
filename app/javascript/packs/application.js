03aa0e131272fb6f064bd43b891866f0b2ec998d
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-05-13 12:04:51 +0900

    application.jsのコメント削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,20 +1,9 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
 
 require("trix")
 require("@rails/actiontext")
@@ -24,19 +13,6 @@ import axios from 'axios'
 import { csrfToken } from 'rails-ujs'
 
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
-
-// window.addEventListener('load', () => {
-//     const uploader = document.querySelector('.uploader');
-//     uploader.addEventListener('change', (e) => {
-//       const file = uploader.files[0];
-//       const reader = new FileReader();
-//       reader.readAsDataURL(file);
-//       reader.onload = () => {
-//         const image = reader.result;
-//         document.querySelector('.avatar').setAttribute('src', image);
-//       }
-//     });
-// });
 
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked) {

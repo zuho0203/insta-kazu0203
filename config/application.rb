4c8445e83acddc84d63b297f796b3063f509cb9c
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-05-13 13:01:44 +0900

    require 'action_controller/railtie' require 'action_view/railtie' を追加

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,9 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require 'action_controller/railtie'
+require 'action_view/railtie'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 if Rails.env.development? || Rails.env.test?

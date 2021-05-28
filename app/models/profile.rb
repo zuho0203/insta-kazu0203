653beaa0efe7c323ac3d0eb6cd18e455df2b32b5
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-05-28 12:18:09 +0900

    avatar修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,16 +3,4 @@ class Profile < ApplicationRecord
     belongs_to :user
     has_one_attached :avatar
 
-
-    def age
-        return '不明' unless birthday.present?
-        years = Time.zone.now.year - birthday.year
-        days = Time.zone.now.yday - birthday.yday
-
-        if days < 0
-            "#{years -1}歳"
-        else
-            "#{years}歳"
-        end
-    end
 end

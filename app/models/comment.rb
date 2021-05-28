70a9f5fc817298b7efd99db3e1c0fbc47f119c65
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-05-28 12:47:00 +0900

    after_create修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,7 @@ class Comment < ApplicationRecord
     validates :content, presence: true
     validates :content, length: { minimum: 1, maximum: 70 }
 
-    after_create :send_email
+    # after_create :send_email
 
     private
     def send_email
